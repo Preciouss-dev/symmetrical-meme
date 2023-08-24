a96a4240084c2569198811fcf440b26b00c54d78
pmyemane021 <pmyemane021@student.wethinkcode.co.za>
2023-08-24 16:45:32 +0200

    removed satabase functionality,can add and view ex

--- main.py
+++ main.py
@@ -1,37 +1,82 @@
-import sqlite3
-import datetime
+from datetime import datetime
 
-connc = sqlite3.connect("expenses.db")
-cur = connc.cursor()
+expenses = []
 
-while True:
-    print("Select an option:")
-    print("1. Enter a new expense")
-    print("2. View expenses summary")
-
-    choice = int(input())
-
-    if choice == 1:
-        date = input("Enter the date (YYY-MM-DD): ")
-        description = input("Enter the description of the expense: ")
-
-        cur.execute("SELECT DISTINCT category FROM expenses")
-
-        categories = cur.fetchall()
-
-        print("Select a category by number:")
-        for idx, category in enumerate(categories):
-            print(f"{idx + 1}. {category[0]}")
-        print(f"{len(categories) +1}. Create a new category")
-
-        category_choice = int(input())
-        if category_choice == len(categories) + 1:
-            category = input("Enter the new category name: ")
-
+def removeExpense():
+    while True:
+        listExpenses()
+        print("What expense would you like to remove?")
+        user_input = input("- ")
+        
+        if user_input.isdigit():
+            expenseToRemove = int(user_input)
+            if 0 <= expenseToRemove < len(expenses):
+                del expenses[expenseToRemove]
+                break
+            else:
+                print("Expense index out of range. Please try again.")
         else:
-            category = categories[category_choice - 1][0]
+            print("Invalid input. Please enter a valid expense index.")
 
-    elif choice == 2:
-        pass
-    else:
-        exit()
+
+def addExpense(amount, category, date):
+    expense = {'amount': amount, 'category': category, 'date': date}
+    expenses.append(expense)
+
+
+def printMenu():
+    print("Please choose from one of the following options...")
+    print("1. Add A New Expense")
+    print("2. Remove An Expense")
+    print("3. List All Expenses")
+
+
+def listExpenses():
+    print("\nHere is a list of your expenses...")
+    print("------------------------------------")
+    counter = 1
+    for expense in expenses:
+        print("*", counter, "- ", expense['amount'], " - ", expense['category'], " - ", expense['date'])
+        counter += 1
+    print("\n\n")
+
+
+if __name__ == "__main__":
+    while True:
+        #get user input
+        printMenu()
+        optionSelected = input("- ")
+
+        if optionSelected == "1":
+            print("How much was this expense? (R)")
+            while True:
+                amountToAdd = input("- ")
+                if amountToAdd.isdigit():
+                    break
+                else:
+                    print("Invalid input. Please try again.")
+
+            print("Which category does this expense fall under?")
+            while True:
+                category = input("- ")
+                if category:
+                    break
+                else:
+                    print("Invalid input. Please try again.")
+
+            print("Enter the date of the expense (YYYY-MM-DD):")
+            while True:
+                date_str = input("- ")
+                try:
+                    date = datetime.strptime(date_str, '%Y-%m-%d').date()
+                    break
+                except ValueError:
+                    print("Invalid date format. Please use YYYY-MM-DD.")
+
+            addExpense(amountToAdd, category, date)
+        elif optionSelected == "2":
+            removeExpense()
+        elif optionSelected == "3":
+            listExpenses()
+        else:
+            print("Invalid input. Please try again.")
